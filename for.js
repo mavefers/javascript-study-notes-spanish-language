c11995abf25a995b406d47b2d3a98de49b5cb481
mavefers <adrianmaverick19@gmail.com>
2022-04-09 02:03:53 -0500

    Updated

--- for.js
+++ for.js
@@ -1,3 +1,5 @@
+//NOTA: 'function' funciona para pasar el parametro, entre otras cosas mas.
+
 // Loops:
 // Es una manera rapida y sencilla de poder hacer que un trabajo se repita de manera automatica dependiendo de una condicion.
 //
@@ -8,10 +10,10 @@
 // Existen varios tipos de "Loops", dependiendo del caso de uso, necesitaremos un tipo u otro.
 // TIPOS DE LOOPS:
 //
-// Tres elementos que controlan el Flujo de ejecusion del bucle:
-// Inicializacion: Fija los valores con los que ejecutamos el blucle.
-// Condicion de Permanencia: Hace que el loop continue hasta que la condicion deje de ser 'true' y la cadena se rompa.
-// Actualizacion de la Variable que controla esa condicion: El loop continua, utilizando diferentes datos. Ejm: i++
+// - Tres elementos que controlan el Flujo de ejecucion del bucle:
+// > Inicializacion: Fija los valores con los que ejecutamos el blucle.
+// > Condicion de Permanencia: Hace que el loop continue hasta que la condicion deje de ser 'true' y la cadena se rompa.
+// > Actualizacion: Actualizacion de la Variable que controla esa condicion: El loop continua, utilizando diferentes datos. Ejm: i++
 
 
 // Loop "while" (mientras):
@@ -80,6 +82,8 @@ function doWhile(num) {
         console.log(i);  //1) El trabajo inicia aqui
         i++     //2) Instruccion que le agrega +1 al valor de la variable.
     } while (i < num);  //3)Se ejecuta la condicion. Si es verdadera, vuelve a realizar el bloque 'do'.
+                        //La funcion de while es que se repita el bloque 'do', siempre y cuando la condicion sea verdadera.
+                        //La funcion de 'while' es hacer que se repita de nuevo el bloque 'do', hasta que la condicion sea falsa.
 }
 doWhile(4);
 //RESULTADO:
@@ -94,9 +98,50 @@ function doWhile(num) {
     let i = 11;
     do {        //Comienza la ejecucion el bloque 'do'.
         console.log(i); //1) Imprime la variable 'i'.
-        i++;        //2) Agrega +1 al valor de la variable.
+        i++;       //2) Instruccion que agrega +1 al valor de la variable.
     } while (i <= num); //3)Revisa la condicion.
 }
+
 doWhile(10); //Este parametro (10) hace que la condicion sea falsa. Fin. // Solo ejecuto la impresion.
 //RESULTADO:
 // 11
+//
+//Notas:
+// Se realizo el bloque 'do'. Se reviso la condicion de 'while'. No se realizo el loop porque la condicion de 'while' es falsa.
+
+
+
+
+
+//          Loop 'for'
+
+//SINTAXIS:
+
+// for(inicializacion; condicion de permanencia; actualizacion) {
+//     codigo que queremos que se ejecute;
+// }
+
+//EJEMPLO
+for (i = 0; i < 3; i++) {
+    console.log(i);
+}
+//RESULTADO:
+//0
+//1
+//2
+
+//Ejemplo2
+function loopFor(num) {
+    for(i=1; i <= num; i++) {
+        console.log(i);
+    }
+}
+
+loopFor(5);
+
+//RESULTADO:
+//1
+//2
+//3
+//4
+//5
