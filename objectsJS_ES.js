c2ecb27bd7d7ca2e341ae3eb39dd8e0bd066d3fb
mavefers <adrianmaverick19@gmail.com>
2022-04-23 13:59:54 -0500

    Updated

---
+++ objectsJS_ES.js
@@ -0,0 +1,24 @@
+//OBJETOS
+//Un objeto es una colección de datos relacionados y/o funcionalidad.
+//Es una representacion abstracta de una cosa de la vida real.
+//Los objetos se manejan con propiedades, y las propiedades son una combinacion de palabras clave y valores.
+//Las llaves indican desde donde inicia y donde termina el objeto. {...}
+//Los números de funcionan como valor, se manejan como datos numéricos, no como string.
+//Al igual que los números, los datos booleanos también se manejan como booleans, no como strings.
+
+//SYNTAX:
+//Están conformados por propiedades y su valor; siendo representados en el ejemplo, las *propiedades* como *palabraClave* y su valor.
+var objeto = {
+    palabraClave: "valor",
+    palabraClave2: 40,
+    palabraClave3: "valor 3"
+};
+
+
+//EXAMPLE:
+var miAuto = {
+    color: "Amarillo",
+    puertas: 4,
+    marca: "Jeep",
+    annio: 2016
+};
