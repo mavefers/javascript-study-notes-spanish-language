da8b23d53ddc436c0ec36ebd8cfedda544a72351
mavefers <adrianmaverick19@gmail.com>
2022-04-27 18:27:56 -0500

    Updated

---
+++ arrayClass2.js
@@ -0,0 +1,35 @@
+//Previamente:
+//Tema: Array
+//Tema: Objetos en JS.
+//Hay dos grupos de Methods de Array principales: -Methods para mutar.   -Methods para recorrer el Array.
+
+//Remember:
+//Array SYNTAX:
+[element0, element1, element2];
+//Objetos SYNTAX:
+{
+    propiedad: valor,
+    propiedad2: valor,
+    propiedad3: valor,
+}
+//Function constructoras:
+function Name(argument1, argument2, argument3) {
+    this.argument1 = argument1;
+    this.argument2 = argument2;
+    this.argument3 = argument3;
+}
+//
+
+
+// Array con Objetos / Objetos dentro de Arrays
+
+//Tema:
+//Methods de recorrido para *obtener propiedades especificas de los objetos que estan dentro de un Array*.
+
+var articulos = [
+    { nombre: "Bici", costo: 3000},
+    { nombre: "Tv", costo: 1500},
+    { nombre: "Libro", costo: 320},
+    { nombre: "Celular", costo: 10000},
+    { nombre: "Laptop", costo: 20000},
+]
