9ae1e7379939917e57152c72cb67a8b261309655
mavefers <adrianmaverick19@gmail.com>
2022-04-27 00:38:48 -0500

    Updated

---
+++ buenasPracticas.js
@@ -0,0 +1,21 @@
+//Sintaxis
+
+//VARIABLES
+
+var x = "string";   //Crea
+x = "texto";    //Reasigna
+
+//Mala practica, volver a crear una variable con el mismo nombre: *var x = "string2";*
+
+
+//
+
+
+//SISTEMAS DE NOMBRADOS
+
+//No afecta en lo absoluto al codigo, es tema de presentacion, organizacion y *buenas practicas*.
+
+// CAMEL CASE:
+//"camelCase" es la práctica de escribir varias palabras sin espacios, puntuación o guiones bajos
+//  dejando la primera letra de cada palabra en mayúscula, excepto la primera.
+
