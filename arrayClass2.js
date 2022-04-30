fe7c3d84d1ef3ffa35be71ba085889c007c1bc3f
mavefers <adrianmaverick19@gmail.com>
2022-04-30 04:12:30 -0500

    Updated

--- arrayClass2.js
+++ arrayClass2.js
@@ -100,3 +100,117 @@ var articulos = [
 var nombreArticulos = articulos.map(function(articulo) {
     return articulo.nombre
 });
+console.log(nombreArticulos);
+//['Bici', 'Tv', 'Libro', 'Celular', 'Laptop', 'Audifonos']
+
+
+
+//Method .find();
+//Nos ayuda a encontrar algo dentro del array y lo almacena en un nuevo array.
+//.find(); devolverá el primer parámetro que coincida con el proceso de validación.
+//Una vez que se obtenga el primer valor, ya no analizará los siguientes valores, y finalizará su proceso. Solo procesa el primero.
+//Recordatorio: El motor de JavaScript procesa el código leyendo desde arriba hacia abajo, o sea, desde la primera linea, hasta la última.
+//Valida un 'true' o un 'false'. Si el elemento existe, lo regresa, y si no existe, no lo regresa (return).
+
+var articulos = [
+    { nombre: "Bici", costo: 3000},
+    { nombre: "Tv", costo: 1500},
+    { nombre: "Libro", costo: 320},
+    { nombre: "Celular", costo: 10000},
+    { nombre: "Laptop", costo: 20000},
+    { nombre: "Laptop", costo: 10000},
+    { nombre: "Laptop", costo: 30000},
+    { nombre: "Audifonos", costo: 1700},
+];
+
+var encuentraArticulo = articulos.find(function(articulo){
+    return articulo.nombre === "Laptop" ;
+})
+console.log(encuentraArticulo);
+//[{nombre: 'Laptop', costo: 20000}]
+
+
+
+//
+
+
+
+//Method .forEach();
+//No genera un nuevo array, solo realiza un filtrado sobre el Array, sin modificarlo, y te retorna los elementos filtrados.
+
+
+var articulos = [
+    { nombre: "Bici", costo: 3000},
+    { nombre: "Tv", costo: 1500},
+    { nombre: "Libro", costo: 320},
+    { nombre: "Celular", costo: 10000},
+    { nombre: "Laptop", costo: 20000},
+    { nombre: "Laptop", costo: 10000},
+    { nombre: "Laptop", costo: 30000},
+    { nombre: "Audifonos", costo: 1700},
+];
+
+articulos.forEach(function(articulo){
+    console.log(articulo.costo);
+});
+
+//3000
+//1500
+//320
+//10000
+//20000
+//10000
+//30000
+//1700
+
+
+
+//
+
+
+
+//Method .son();
+//Sirve para comprobar si al menos un elemento del array cumple con la condición interpuesta por la función realizada.
+//Realiza una validación de 'true' o 'false' (verdadero o falso), analizando cada elemento,
+// y retorna si la condición es true o es false.
+//Solo se necesita que al menos un elemento sea 'true'/verdad para que se valide como verdadero/'true'.
+// Si no hay ningún elemento que cumpla con la condición interpuesta, nos retornaría un 'false'.
+// No retorna ningún elemento. 
+
+var articulos = [
+    { nombre: "Bici", costo: 3000},
+    { nombre: "Tv", costo: 1500},
+    { nombre: "Libro", costo: 320},
+    { nombre: "Celular", costo: 10000},
+    { nombre: "Laptop", costo: 20000},
+    { nombre: "Laptop", costo: 10000},
+    { nombre: "Laptop", costo: 30000},
+    { nombre: "Audifonos", costo: 1700},
+];
+
+var articulosBaratos = articulos.some(function(articulo) {
+    return articulo.costo >= 10000;
+});
+
+console.log(articulosBaratos);
+//true
+
+
+// - Caso de un ejemplo real:
+// Si un usuario que le interese comprar en una e-commerce, quiere validar si hay artículos que cumplan con cierta especificación,
+// artículos que tengan alguna promoción, articulos que sean de cierta marca, o de cierto rango de precio; se podría filtrar usando .filter();,
+// se le retornaría la validación de 'true' o 'false', usando .some();, para asi inidicarle si hay o no hay cierto artículo,
+// y así mostrarle cuáles son esos artículos, usando el método .forEach();
+
+
+// RESUMEN DEFINITIVO:
+//Es importante conocer como funcionan los métodos para no confundirlos y sacarles el máximo provecho.
+//Las diferencias más importantes son:
+//El .forEach() NO crea un nuevo array y el .map() sí lo hace.
+//El .filter() crea un nuevo array con los objetos que cumplan una condición,
+//el .some() solamente indica si hay o no algún objeto que cumpla dicha condición.
+//El .find() devuelve SOLO el primer objeto que cumpla con la condición.
+//  Ejemplo: Si en tu array de artículos tienes una 'bicicleta' de $800 y luego un celular de $800,
+//           si usas un .find() para buscar un artículo de $800 como costo, solo aparecerá la 'bicicleta',
+//           si deseas ambos tienes que usar un .filter();.
+//          -JavaScript lee las lineas de código, desde arriba hacia abajo.
