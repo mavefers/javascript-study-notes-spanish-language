fae99bc15d09272e0d219930016577e9f5684836
mavefers <adrianmaverick19@gmail.com>
2022-05-04 04:36:45 -0500

    Updated

---
+++ practicesOne/reviewJS_Basic.js
@@ -0,0 +1,8 @@
+// Repaso - Review
+
+//Variable:
+//Una variable es un espacio en memoria, sirve para almacenar todo tipo de dato.
+
+//Diferencia entre declarar e iniciar una variable:
+//Al declarar una variable, comienzas a crear un espacio en memoria, al cual le das un nombre como identificador.
+//Cuando inicializas una variable, estás otorgando un valor a un espacio vacío en memoria, que vendría a ser una variable no inicializada.
