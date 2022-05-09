9dc0bb57ec8ba76c40326c7d95d092cb7a540cef
mavefers <adrianmaverick19@gmail.com>
2022-05-09 13:55:22 -0500

    Updated

--- 3. practicesOne/reviewJS_Basic.js
+++ 3. practicesOne/reviewJS_Basic.js
@@ -1,13 +1,52 @@
-// Repaso - Review
+/*
+Repaso - Review
 
-//Variable:
-//Una variable es un espacio en memoria, sirve para almacenar todo tipo de dato.
-
-//Diferencia entre declarar e iniciar una variable:
-//Al declarar una variable, comienzas a crear un espacio en memoria, al cual le das un nombre como identificador.
-//Cuando inicializas una variable, estás otorgando un valor a un espacio vacío en memoria, que vendría a ser una variable no inicializada.
-
-
-//
+1.
+-Variable:
+Una variable es un espacio en memoria, sirve para almacenar todo tipo de dato o valor.
+Es la representacion de algun lugar en memoria que nosotros vamos a reservar para poder guardar un valor de diferentes tipos.
 
 
+-Diferencia entre declarar e iniciar una variable:
+Al declarar una variable, comienzas a crear un espacio en memoria, al cual le das un nombre como identificador. Ejm: 'var pack;'
+Cuando inicializas una variable, estás otorgando un valor a un espacio vacío en memoria, que vendría a ser una variable no inicializada. Ejm: 'var pack = 47;'
+
+
+Diferencia entre sumar numeros y concatenar strings:
+-Sumar numeros es un calculo matematico que retorna un nuevo valor, en base al tipo de operacion que se esta realizando.
+-Concatenar Strings es la accion de agrupar valores de tipo texto, siendo combinados en un nuevo valor; si los valores concatenados son numeros, pasan a convertirse en strings.
+
+
+Operador que permite sumar o concatenar:
+- El signo de suma, +.
+-   Otros operadores para realizar concatenaciones son   `texto ${variable} texto`
+
+
+2.
+Nombre, variable tipo string = 'Tomas'
+Apellido, variable tipo string = 'Thomson'
+Nombre de usuario en Platzi, variable tipo string = 'Tom'
+Edad, variable tipo number = 32
+Correo electronico, variable tipo string = hola@mundo.com
+Mayor de edad, variable tipo Boolean = true
+Dinero ahorrado, variable tipo number = 19000
+Deudas, variable tipo Number = 4000
+
+
+3.
+let name = 'Tomas';
+let lastname = 'Thomson';
+let usernamePlatzi = 'Tom';
+let age = 32;
+let email = 'hola@mundo.com';
+let adult = true;
+let savedMoney = 19000;
+let debts = 4000;
+
+
+4.
+let nombreCompleto = name + ' ' + lastname; || let nombreCompleto = `${name} ${lastname}`;
+let dineroNeto = savedMoney - debts;
+
+
+*/
